03b70a7a11bbe3853a8253eed9cdb23cef2ffe71
Lisa Vader <lisvad@dtu.dk>
2025-11-28 15:42:51 +0100

    trainer: Don't add additional flanks based on CL, flanks are already added when building annotation tsv

--- trainer/create_datafile.sh
+++ trainer/create_datafile.sh
@@ -3,14 +3,13 @@
 set -ex
 
 G=$1
-CL=$2
-DATA_DIR=$(realpath ${3:=./})
-TSV_DIR=$(realpath ${4:=./})
-FASTA_DIR=$(realpath ${5:=./})
+DATA_DIR=$(realpath ${2:=./})
+TSV_DIR=$(realpath ${3:=./})
+FASTA_DIR=$(realpath ${4:=./})
 echo DATA_DIR: $DATA_DIR
 mkdir -p $DATA_DIR
 
-if [ -f "$DATA_DIR/datafile_train_${CL}_${G}.h5" ]; then
+if [ -f "$DATA_DIR/datafile_train_${G}.h5" ]; then
     exit
 fi
 
@@ -18,12 +17,11 @@ genome_path=$FASTA_DIR/*$G*.f*a
 samtools faidx $genome_path
 
 export splice_table=$TSV_DIR/$G.tsv
-export CL_max=$CL
 export ref_genome=$genome_path
 export data_dir=$DATA_DIR
 export sequence=$DATA_DIR/${G}_canonical_sequence.txt
 bash trainer/grab_sequence.sh
 
-python trainer/create_datafile.py train $CL_max $G $data_dir $sequence $splice_table
-python trainer/create_datafile.py test $CL_max $G $data_dir $sequence $splice_table
+python trainer/create_datafile.py train $G $data_dir $sequence $splice_table
+python trainer/create_datafile.py test $G $data_dir $sequence $splice_table
 
--- trainer/grab_sequence.sh
+++ trainer/grab_sequence.sh
@@ -2,11 +2,8 @@
 
 echo processing $splice_table $ref_genome
 
-CLr=$((CL_max/2))
-CLl=$(($CLr+1))
 # First nucleotide not included by BEDtools
-
-cat $splice_table | awk -v CLl=$CLl -v CLr=$CLr '{print $3"\t"($5-CLl)"\t"($6+CLr)}' > $splice_table.bed
+cat $splice_table | awk '{print $3"\t"($5-1)"\t"$6}' > $splice_table.bed
 
 bedtools getfasta -bed $splice_table.bed -fi $ref_genome -fo $sequence -tab
 
